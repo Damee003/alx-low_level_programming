9c427bbe279adefd0ff27b3dddffa5b90f343629
root <ojetolaoluwadamilola@gmail.com>
2022-09-08 17:01:34 -0700

    Wink

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
- *
  * * main - main block
- *
  * * description - the starting point of the program
- *
  * * Return: 0
- *
  * */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
