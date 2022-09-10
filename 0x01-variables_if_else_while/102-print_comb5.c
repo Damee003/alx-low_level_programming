b4d4136aa1e925c86b5b05f63849ef734ec8318e
root <ojetolaoluwadamilola@gmail.com>
2022-09-10 12:02:58 -0700

    Pie

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - assign a random number to the variable n each time it is executed 
-*and print the last digit of the number stored in the variable n
-*Return: Always 0 (Success)
+* main - this function prints possible combination of two 2-digit
+* numbers
+* Return: returns zero at the end
 */
 int main(void)
 {
@@ -11,14 +11,19 @@ for (i = 0; i < 100; i++)
 {
 for (j = 0; j < 100; j++)
 {
+if (i < j)
+{
 putchar((i / 10) + 48);
 putchar((i % 10) + 48);
 putchar(' ');
 putchar((j / 10) + 48);
 putchar((j % 10) + 48);
 if (i != 98 || j != 99)
+{
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 putchar('\n');
