86a79f37be724f45f75257410164866da0f73cac
root <ojetolaoluwadamilola@gmail.com>
2022-09-09 20:22:20 -0700

    Sugar

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,16 +13,12 @@ for (m = 49; m < 58; m++)
 for (l = 50; l < 58; l++)
 {
 if (l > m && m > n)
-{
 putchar(n);
 putchar(m);
 putchar(l)
 if (n != 55 || m != 56)
-{
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 }
