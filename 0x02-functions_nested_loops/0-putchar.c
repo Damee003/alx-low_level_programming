c6126668985b96a53e78487b2957fc8dbda99515
root <ojetolaoluwadamilola@gmail.com>
2022-09-13 15:21:29 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
 *main - prints Putchar
-*Description: prints _putchar
 *Return: 0 (Success)
 */
+
 int main(void)
 {
 _putchar('');
@@ -15,5 +16,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
