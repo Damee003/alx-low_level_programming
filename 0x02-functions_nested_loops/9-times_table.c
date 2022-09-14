67d61a646a57e0f7896b52d2b45671aab1e2bb05
root <ojetolaoluwadamilola@gmail.com>
2022-09-14 08:28:03 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,23 +14,23 @@ for (j = 0; j <= 9; j++)
 int prod = j * i;	
 if (j == 0)
 {
-	_putchar('0');
+_putchar('0');
 }
 else if (prod <= 9)
 {
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(prod + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(prod + '0');
 }
 else
 {
-	_putchar(',');
-	_putchar(' ');
-	_putchar(prod / 10 + '0');
-	_putchar(prod % 10 + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(prod / 10 + '0');
+_putchar(prod % 10 + '0');
 }
 }
-	_putchar('\n');
+_putchar('\n');
 }
 }
