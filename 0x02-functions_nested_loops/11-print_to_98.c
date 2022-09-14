5c5380ef5bc8056f789e24c935494b0a079ddca2
root <ojetolaoluwadamilola@gmail.com>
2022-09-13 17:41:40 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,14 +5,15 @@
 */
 void print_times_table(int n)
 {
-int i;
-int j;
+	int i;
+	int j;
 	if (n <= 15 && n >= 0)
 {
 	for (i = 0; i <= n; i++)
 {
 	for (j = 0; j <= n; j++)
-{										int prod = j * i;
+{
+	int prod = j * i;
 	if (j == 0)
 {
 	_putchar('0');
