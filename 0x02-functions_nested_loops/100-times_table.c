b857deeb695aa72f05f5c1fcd43e01677e7d226c
root <ojetolaoluwadamilola@gmail.com>
2022-09-14 11:29:35 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,17 +3,17 @@
 *print_times_table - times table function
 *@n: integer to return
 */
-	void print_times_table(int n)
+void print_times_table(int n)
 {
-	int i;
-	int j;
+int i;
+int j;
 if (n <= 15 && n >= 0)
 {
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
-	int prod = j * i;
+int prod = j * i;
 if (j == 0)
 {
 _putchar('0');
