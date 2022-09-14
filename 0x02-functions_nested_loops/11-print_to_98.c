e56f5842f678a4185fe1b05eba8b5e02ed86b523
root <ojetolaoluwadamilola@gmail.com>
2022-09-14 10:13:50 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,26 +6,26 @@
 *@n: input number to check
 *Return: nothing
 */
-void print_to_98(int n);
+void print_to_98(int n)
 {
 int i;
-if (n > 98)
+	if (n > 98)
 {
-for (i = n; i >= 98; i--)
+	for (i = n; i >= 98; i--)
 {
 printf("%d", i);
-if (i != 98)
+	if (i != 98)
 {
 printf(", ")
 }
 }
 }
-else
+	else
 {
-for (i = n; i <= 98; i++)
+	for (i = n; i <= 98; i++)
 {
 printf("%d", i);
-if (i != 98)
+	if (i != 98)
 {
 printf(", ");
 }
