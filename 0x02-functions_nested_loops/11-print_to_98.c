45ab68fa668fed5aa966620d568656651c4ff792
root <ojetolaoluwadamilola@gmail.com>
2022-09-14 08:54:32 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* print_times_table - times table function
-* @n: integer to return
+*print_times_table - times table function
+*@n: integer to return
 */
 
 void print_times_table(int n)
