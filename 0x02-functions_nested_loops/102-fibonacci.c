6019d11296697c9bc9ccf563d3e2ea4055c32dfb
root <ojetolaoluwadamilola@gmail.com>
2022-09-13 18:14:58 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,10 +6,11 @@
 
 int main(void)
 {
-	int counter = 0;
-	long int a = 1;
-	long int b = a;
-	long int c = a + b;
+int counter = 0;
+long int a = 1;
+long int b = a;
+long int c = a + b;
+
 	while (c < 4000000)
 {
 	if (c % 2 == 0)
