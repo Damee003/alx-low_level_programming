28476b923a4c995b83c12809a4d9a0cf04810f88
root <ojetolaoluwadamilola@gmail.com>
2022-09-14 08:42:52 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * print_times_table - times table function
 * @n: integer to return
@@ -9,44 +8,43 @@ void print_times_table(int n)
 {
 int i;
 int j;
-
-	if (n <= 15 && n >= 0)
+if (n <= 15 && n >= 0)
 {
-	for (i = 0; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
-	for (j = 0; j <= n; j++)
+for (j = 0; j <= n; j++)
 {
 int prod = j * i;
-	if (j == 0)
+if (j == 0)
 {
-	_putchar('0');
+_putchar('0');
 } 
-	else if (prod <= 9)
+else if (prod <= 9)
 {
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar('0' + prod);
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + prod);
 }
-	else if (prod > 99)
+else if (prod > 99)
 {
-	_putchar(',');
-	_putchar(' ');
-	_putchar('0' + (prod / 100));
-	_putchar('0' + ((prod / 10) % 10));
-	_putchar('0' + (prod
+_putchar(',');
+_putchar(' ');
+_putchar('0' + (prod / 100));
+_putchar('0' + ((prod / 10) % 10));
+_putchar('0' + (prod
 }
-	else
+else
 {
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar('0' + (prod / 10));
-	_putchar('0' + (prod % 10));
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + (prod / 10));
+_putchar('0' + (prod % 10));
 }
 }
-	_putchar('\n');
+_putchar('\n');
 }
 }
 }
