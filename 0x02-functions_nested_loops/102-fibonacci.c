79596ee7d4dc2160bb608ac6d62ff9119b6245e6
root <ojetolaoluwadamilola@gmail.com>
2022-09-13 18:28:16 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
 * main - main function
+*
 * Return: nothing
 */
 
 int main(void)
 {
-int counter = 0;
-long int a = 1;
-long int b = a;
-long int c = a + b;
+	int counter = 0;
+	long int a = 1;
+	long int b = a;
+	long int c = a + b;
 	while (c < 4000000)
 {
 	if (c % 2 == 0)
@@ -21,6 +22,5 @@ long int c = a + b;
 	c = a + b;
 }
 	printf("%d\n", counter);
-	
 	return (0);
 }
