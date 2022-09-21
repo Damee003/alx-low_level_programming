dd34b2b7352082569b3d2c81e7ffaa1a5d45b834
root <ojetolaoluwadamilola@gmail.com>
2022-09-21 16:48:30 -0700

    It is well

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,6 +7,7 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
+
 /*
 * write your line of code here...
 * Remember:
@@ -15,7 +16,9 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
+
 *(p + 5) = 98;
+
 /*
 * ...so that this prints 98\n
 */
