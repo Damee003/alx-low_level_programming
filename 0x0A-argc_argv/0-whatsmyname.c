c076e7a66fb3f5cd6890f2eda0248a80d9cf206e
root <ojetolaoluwadamilola@gmail.com>
2022-09-30 15:33:33 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,14 @@
 #include "main.h"
 
 /**
-* main - print the name of the program
-* @argc: Counts argument
-* @argv: Argument vector
+* main - Print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
 * Return: Always 0 (Success)
 */
-
-int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
 {
-
-	(void)argc
-	printf("%s\n", argv[0]);
-	
-	return (0);
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
 }
