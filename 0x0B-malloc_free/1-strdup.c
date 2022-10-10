635514f457a875614afb44a25021d3263756cb19
root <ojetolaoluwadamilola@gmail.com>
2022-10-10 07:31:31 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-*_strlen - count arrray
+*_strlen - count array
 *@s: array of elements
-*Return: i
+*Return: 1
 */
+
 int _strlen(char *s)
 {
 unsigned int i;
 i = 0;
-while (s[i] != '\0') /*Count character of string*/
+while (s[i] != '\0') /*count character of string*/
 {
 i++;
 }
@@ -17,35 +18,43 @@ return (i);
 }
 
 /**
-*str_concat - back a pointer to array
-*@s1: Array one
-*@s2: Array two
-*Return: Always an array dinamic
+*_strcpy - copy arrays
+*@src: array of elements
+*@dest: dest array
+*Return: dest
 */
-char *str_concat(char *s1, char *s2)
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
+
+/**
+*_strdup - array for prints a string
+*@str: array of elements
+*Return: pointer
+*/
+char *_strdup(char *str)
 {
 char *dst;
-unsigned int i, j, size;
-/*If the array is empty*/
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-/*count size total*/
-size = (_strlen(s1) + _strlen(s2) + 1);
-/*malloc*/
-dst = (char *) malloc(size * sizeof(char));
+unsigned int size;
+if (str == 0)
+{
+return (NULL);
+}
+size = _strlen(str) + 1;
+dst = (char *) malloc(size *sizeof(char));
 if (dst == 0)
 {
 return (NULL);
 }
-/*Concatenate arrays*/
-for (i = 0; *(s1 + i) != '\0'; i++)
-*(dst + i) = *(s1 + i);
-for (j = 0; *(s2 + j) != '\0'; j++)
-{
-*(dst + i) = *(s2 + j);
-i++;
-}
+_strcpy(dst, str);
 return (dst);
 }
