1d1aee7eed8b5de50f4627848c60e5fd88453f66
root <ojetolaoluwadamilola@gmail.com>
2022-10-10 12:26:42 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,36 +12,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 int i = 0, j = 0, k;
-dog_t *doge;
+dog_t *dog;
 while (name[i] != '\0')
 i++;
 while (owner[j] != '\0')
 j++;
-doge = malloc(sizeof(dog_t));
-if (doge == NULL)
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
 {
-free(doge);
+free(dog);
 return (NULL);
 }
-doge->name = malloc(i * sizeof(doge->name));
-if (doge->name == NULL)
+dog->name = malloc(i * sizeof(dog->name));
+if (dog->name == NULL)
 {
-free(doge->name);
-free(doge);
+free(dog->name);
+free(dog);
 return (NULL);
 }
 for (k = 0; k <= i; k++)
-doge->name[k] = name[k];
-doge->age = age;
-doge->owner = malloc(j * sizeof(doge->owner));
-if (doge->owner == NULL)
+dog->name[k] = name[k];
+dog->age = age;
+dog->owner = malloc(j * sizeof(dog->owner));
+if (dog->owner == NULL)
 {
-free(doge->owner);
-free(doge->name);
-free(doge);
+free(dog->owner);
+free(dog->name);
+free(dog);
 return (NULL);
 }
 for (k = 0; k <= j; k++)
-doge->owner[k] = owner[k];
-return (doge);
+dog->owner[k] = owner[k];
+return (dog);
 }
