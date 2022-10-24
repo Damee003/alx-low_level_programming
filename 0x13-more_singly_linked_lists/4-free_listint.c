574c70c75a1d12d52bff2133ec24bcc42ffe945d
root <ojetolaoluwadamilola@gmail.com>
2022-10-24 14:59:11 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,12 +5,13 @@
 * @head: listint_t list to be freed
 */
 void free_listint(listint_t *head)
+
 {
 listint_t *temp;
 while (head)
 {
 temp = head->next;
 free(head);
-head = temp
+head = temp;
 }
 }
